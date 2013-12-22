a4779e2fbb60e36c9a3bbb9afea590d6ef562da8
MarcosAlarconRivas <marcos.alarcon@ucm.es>
2013-12-22 21:03:36 +0100

    Added QT Skeleton

---
+++ Skeleton/Skeleton.pro
@@ -0,0 +1,12 @@
+QT          += opengl
+
+HEADERS     = glwidget.h \
+              helper.h \
+              window.h
+SOURCES     = glwidget.cpp \
+              helper.cpp \
+              main.cpp \
+              window.cpp
+
+# install
+INSTALLS += target
---
+++ Skeleton/window.cpp
@@ -0,0 +1,15 @@
+#include "glwidget.h"
+#include "window.h"
+
+#include <QTimer>
+
+Window::Window()
+{
+    setWindowTitle(tr("openGL Widget"));
+
+    GLWidget *openGL = new GLWidget(&helper, this);
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), openGL, SLOT(animate()));
+    timer->start(50);
+}
