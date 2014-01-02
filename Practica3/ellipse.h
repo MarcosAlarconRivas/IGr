e15a83be07875426c83cfcc9038a7da28d459c9a
MarcosAlarconRivas <marcos.alarcon@ucm.es>
2014-01-02 20:20:32 +0100

    Cleaned Ellipse

--- Practica3/ellipse.h
+++ Practica3/ellipse.h
@@ -2,12 +2,10 @@
 #define ELLIPSE_H
 #include "circle.h"
 #include "v2d.h"
-#include <GL/gl.h>
 
 class Ellipse : public Circle{
 protected:
-    double width, height, rotation;V2d cent;
-    GLdouble transf[16];
+    double transf[16];
     double tr_inv[16];
 
 public:
@@ -15,9 +13,6 @@ public:
     virtual void paint()const;
     virtual bool intersection(V2d p, V2d d, float s, double& tIn, V2d& normalIn)const;
 
-    static void invert(double matrix[4][4], double result[4][4]);
-    static inline double determinant(double matrix[4][4]);
-    static inline void post_mult(float v[4], const double matrix[16]);
     static inline void pre_mult(const double matrix[16], float v[4]);
 
 };
