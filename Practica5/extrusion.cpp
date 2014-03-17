ee38a3f80cbd6f56e573e051097bf1dae7320473
MarcosAlarconRivas <marcos.alarcon@ucm.es>
2014-03-17 23:33:57 +0100

    Implemeted poligon

--- Practica5/extrusion.cpp
+++ Practica5/extrusion.cpp
@@ -1,18 +1,23 @@
 #include "extrusion.h"
 
 //private fuction to make a regular poligon of @sides points and @rad size
-static vector<vtx>& poligon(unsigned rad, unsigned sides){
-    auto pol= vector<vtx>(sides);
-    for(unsigned i=0; i<sides; i++){
-      //TODO add vertex
+static vector<v2d>& poligon(unsigned rad, unsigned sides){
+    static vector<v2d> pol= vector<v2d>(sides);
+    double inc = 2*M_PI/sides;
+    double alpha=0;
+    for(unsigned i=0; i<sides; i++, alpha+=inc){
+      v2d p;
+      p.x= sin(alpha)*rad;
+      p.y= cos(alpha)*rad;
+      pol[i]= p;
     }
     return pol;
 }
 
-Extrusion::Extrusion(unsigned r, unsigned s, V3D(*f)(float), unsigned n)
-    :Extrusion(poligon(r,s), f, n){}
+Extrusion::Extrusion(unsigned r, unsigned s, V3D(*f)(float), unsigned n, double t0, double tf)
+    :Extrusion(poligon(r,s), f, n, t0, tf){}
 
-Extrusion::Extrusion(vector<vtx> cut, V3D(*f)(float), unsigned num){
+Extrusion::Extrusion(vector<v2d> cut, V3D(*f)(float), unsigned num, double t0, double tf){
 
 }
 
--- Practica5/extrusion.h
+++ Practica5/extrusion.h
@@ -1,11 +1,14 @@
 #ifndef EXTRUSION_H
 #define EXTRUSION_H
 #include "mesh.h"
+#include <math.h>
+
+typedef struct{float x; float y;} v2d;
 
 class Extrusion : public Mesh{
 public:
-    Extrusion(vector<vtx> cut, V3D(*curve)(float), unsigned numOfCuts);
-    Extrusion(unsigned raduis, unsigned sides, V3D(*curve)(float), unsigned numOfCuts);
+    Extrusion(vector<v2d> cut, V3D(*curve)(float), unsigned numOfCuts, double from=0, double to=2*M_PI);
+    Extrusion(unsigned raduis, unsigned sides, V3D(*curve)(float), unsigned numOfCuts, double from=0, double to=2*M_PI);
 };
 
 #endif // EXTRUSION_H
