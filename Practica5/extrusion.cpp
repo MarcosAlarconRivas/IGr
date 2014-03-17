910d5d85329988df4286d9c44769c4b7f44c9a69
MarcosAlarconRivas <marcos.alarcon@ucm.es>
2014-03-18 00:48:14 +0100

    Implemented normals for cut

--- Practica5/extrusion.cpp
+++ Practica5/extrusion.cpp
@@ -5,19 +5,36 @@ static vector<v2d>& poligon(unsigned rad, unsigned sides){
     static vector<v2d> pol= vector<v2d>(sides);
     double inc = 2*M_PI/sides;
     double alpha=0;
-    for(unsigned i=0; i<sides; i++, alpha+=inc){
-      v2d p;
-      p.x= sin(alpha)*rad;
-      p.y= cos(alpha)*rad;
-      pol[i]= p;
-    }
+    for(unsigned i=0; i<sides; i++, alpha+=inc)
+      pol[i]= v2d{ sin(alpha)*rad, cos(alpha)*rad};
+
     return pol;
 }
 
-Extrusion::Extrusion(unsigned r, unsigned s, V3D(*f)(float), unsigned n, double t0, double tf)
+Extrusion::Extrusion(unsigned r, unsigned s, V3D(*f)(double), unsigned n, double t0, double tf)
     :Extrusion(poligon(r,s), f, n, t0, tf){}
 
-Extrusion::Extrusion(vector<v2d> cut, V3D(*f)(float), unsigned num, double t0, double tf){
+Extrusion::Extrusion(vector<v2d> cut, V3D(*f)(double), unsigned num, double t0, double tf){
+    if(tf<t0){double x=t0; t0=tf; tf=x;} //begin < end
+    unsigned s= cut.size();  //num of sides in each cut
+    double step= (tf-t0)/num;//inc in each step of curve
+
+    //calculo de las normales
+    vector<v2d> norm = vector<v2d>(s);
+    for(unsigned i=0; i<s; i++){
+        v2d p= cut[(i+s-1)%s];
+        v2d t= cut[i];
+        v2d n= cut[(i + 1)%s];
+        float dx= t.x-p.x + t.x-n.x;
+        float dy= t.y-p.y + t.y-n.y;
+        float mod= sqrt(dx*dx +dy*dy);
+        norm[i]= v2d{dx/mod, dy/mod};
+    }
+
+    double t=t0;
+    for(unsigned i=0; i<num; i++, t+=step){
+        //TODO
+    }
 
 }
 
--- Practica5/extrusion.h
+++ Practica5/extrusion.h
@@ -7,8 +7,8 @@ typedef struct{float x; float y;} v2d;
 
 class Extrusion : public Mesh{
 public:
-    Extrusion(vector<v2d> cut, V3D(*curve)(float), unsigned numOfCuts, double from=0, double to=2*M_PI);
-    Extrusion(unsigned raduis, unsigned sides, V3D(*curve)(float), unsigned numOfCuts, double from=0, double to=2*M_PI);
+    Extrusion(vector<v2d> cut, V3D(*curve)(double), unsigned numOfCuts, double from=0, double to=2*M_PI);
+    Extrusion(unsigned raduis, unsigned sides, V3D(*curve)(double), unsigned numOfCuts, double from=0, double to=2*M_PI);
 };
 
 #endif // EXTRUSION_H
